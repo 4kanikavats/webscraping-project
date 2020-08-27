2d994a54b261ad1388b484bdcd7154244a371a13
4kanikavats <63115132+4kanikavats@users.noreply.github.com>
2020-08-27 13:34:55 +0530

    Add files via upload
    In this project, I've written a script through which I'm able to obtain sorted news lists with their title and links based on the highest votes given by other readers. In this, I have used a website named hacker news and I've used BeautifulSoup library and its various features.

---
+++ Scrap.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup = BeautifulSoup(res.text,'html.parser')
+soup2 = BeautifulSoup(res2.text,'html.parser')
+links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
+subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links+links2
+mega_subtext = subtext+subtext2
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist,key= lambda k:k['votes'],reverse=True)
+def create_custom_hn(links,subtext):
+    hn=[]
+    for idx,item in enumerate(links):
+        title =links[idx].getText()
+        href = links[idx].get('href',None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points =int(vote[0].getText().replace(' points',''))
+            hn.append({'title':title,'links':href,'votes':points})
+    return sort_stories_by_votes(hn)
+pprint.pprint(create_custom_hn(mega_links,mega_subtext))
